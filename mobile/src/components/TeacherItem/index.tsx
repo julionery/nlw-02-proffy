ec9a50ede2a15200dfdcbdcbd4da66d40c91028f
Júlio Nery <julio_cesar.an@hotmail.com>
2020-08-08 20:01:22 -0300

    add module mobile

---
+++ mobile/src/components/TeacherItem/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+
+import styles from './styles';
+
+function TeacherItem() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.profile}>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri:
+              'https://avatars0.githubusercontent.com/u/15279868?s=460&u=cea0e3bd83ae804a2e40a479b94b7eb7d585fd16&v=4',
+          }}
+        />
+
+        <View style={styles.profileInfo}>
+          <Text style={styles.name}>Júlio Nery</Text>
+          <Text style={styles.subject}>Programação</Text>
+        </View>
+      </View>
+
+      <Text style={styles.bio}>
+        Entusiasta das melhores tecnologias de química avançada.
+        {'\n'}
+        {'\n'}
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </Text>
+
+      <View style={styles.footer}>
+        <Text style={styles.price}>
+          {'Preço/hora    '}
+          <Text style={styles.priceValue}>R$ 80,00 </Text>
+        </Text>
+
+        <View style={styles.buttonsContainer}>
+          <RectButton style={[styles.favoriteButton, styles.favorited]}>
+            {/* <Image source={heartOutlineIcon} /> */}
+            <Image source={unfavoriteIcon} />
+          </RectButton>
+
+          <RectButton style={styles.contactButton}>
+            <Image source={whatsappIcon} />
+            <Text style={styles.contactButtonText}>Entrar em contato</Text>
+          </RectButton>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default TeacherItem;
