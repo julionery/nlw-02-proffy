1131c89f8961e3174ebc1e9dea9810ad5f9482d1
Júlio Nery <julio_cesar.an@hotmail.com>
2020-08-08 21:35:21 -0300

    :tada: finalizing the mobile app

---
+++ mobile/README.md
@@ -0,0 +1,72 @@
+<h3 align="center">
+     <img alt="Logo" title="#logo" height="150px" src="https://github.com/julionery/docs/blob/master/NLW-02/logo.png?raw=true">
+</h3>
+<p align="center">
+  <a href="https://rocketseat.com.br">
+    <img alt="Made by Rocketseat" src="https://img.shields.io/badge/made%20by-Rocketseat-%237519C1">
+  </a>
+  <a>
+ <img alt="License" src="https://img.shields.io/github/license/julionery/nlw-01-ecoleta?color=%237519C1">
+</p>
+<h2 align="center">Aplicação Mobile</h2>
+
+Este repositório contém a Aplicação Mobile do projeto [Proffy](https://github.com/julionery/nlw-02-proffy).
+
+### :bookmark_tabs: Funcionalidades:
+- [x] Lista de professores
+- [x] Lista de favoritos
+- [x] Filtro de professores
+- [x] Conectando com a API
+  - [x] Total de conexões
+  - [x] Lista de professores
+- [x] Contato via Whatsapp
+- [x] Adicionando e removendo favoritos
+- [x] Listando favoritos
+- [x] Desafios
+
+### :pushpin: Pre-requisitos:
+- [Ecoleta API](https://github.com/julionery/nlw-02-proffy/tree/master/server)
+
+### :rocket: Tecnologias
+ - [React Native](https://reactnative.dev/ "React Native")
+ - [TypeScript](https://www.typescriptlang.org/)
+
+### :computer: Bibliotecas e ferramentas
+ - [Expo](https://expo.io/ "Expo")
+ - [React Navigation](https://reactnavigation.org/ "React Navigation")
+ - [Axios](https://github.com/axios/axios "Axios")
+ - [Async Storage](https://docs.expo.io/versions/latest/sdk/async-storage/)
+
+### :globe_with_meridians: API's
+ - [Proffy](https://github.com/julionery/nlw-02-proffy/tree/master/server)
+
+### :information_source: Como Usar
+
+Para executar corretamente esta aplicação você precisará do [Git](https://git-scm.com), [NodeJS](https://nodejs.org/en/) já instalados. E da aplicação [(Servidor - API)](https://github.com/julionery/nlw-02-proffy/tree/master/server) devidamente configurada.
+
+No seu terminal digite os comandos:
+
+```bash
+# Clone este repositório
+$ git clone https://github.com/julionery/nlw-02-proffy.git
+
+# Vá para a pasta do repositório
+$ cd nlw-02-proffy/mobile/
+
+# Instale as dependências
+$ npm install
+
+# Inicie a aplicação
+$ npm start
+
+```
+
+## :memo: Licença
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE) para mais detalhes.
+
+---
+
+<h4 align="center">
+    Feito com ❤ por <a href="https://www.linkedin.com/in/julio-nery/" target="_blank">Júlio Nery</a>!
+    <g-emoji class="g-emoji" alias="wave" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/1f44b.png">👋</g-emoji>
+</h4>
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -2,34 +2,75 @@ import React, { useState } from 'react';
 import { View, ScrollView, Text, TextInput } from 'react-native';
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
+import { useFocusEffect } from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import api from '../../services/api';
 
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 
 import styles from './styles';
 
 function TeacherList() {
+  const [teachers, setTeachers] = useState([]);
+  const [favorites, setFavorites] = useState<number[]>([]);
+
   const [isFiltersVisible, setIsFilterVisible] = useState(false);
+
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
+
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites').then(response => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+        const favoritedTeachersIds = favoritedTeachers.map(
+          (teacher: Teacher) => {
+            return teacher.id;
+          },
+        );
+        setFavorites(favoritedTeachersIds);
+      }
+    });
+  }
+
+  useFocusEffect(() => {
+    loadFavorites();
+  });
 
   function handleToggleFiltersVisible() {
     setIsFilterVisible(!isFiltersVisible);
   }
 
+  async function handleFiltersSubmit() {
+    loadFavorites();
+
+    const response = await api.get('classes', {
+      params: { subject, week_day, time },
+    });
+
+    setIsFilterVisible(false);
+    setTeachers(response.data);
+  }
+
+  const filterButton = (
+    <BorderlessButton onPress={handleToggleFiltersVisible}>
+      <Feather name="filter" size={20} color="#FFF" />
+    </BorderlessButton>
+  );
+
   return (
     <View style={styles.container}>
-      <PageHeader
-        title="Proffys disponíveis"
-        headerRight={
-          <BorderlessButton onPress={handleToggleFiltersVisible}>
-            <Feather name="filter" size={20} color="#FFF" />
-          </BorderlessButton>
-        }
-      >
+      <PageHeader title="Proffys disponíveis" headerRight={filterButton}>
         {isFiltersVisible && (
           <View style={styles.searchForm}>
             <Text style={styles.label}>Matéria</Text>
             <TextInput
               style={styles.input}
+              value={subject}
+              onChangeText={text => setSubject(text)}
               placeholder="Qual a matéria?"
               placeholderTextColor="#c1bccc"
             />
@@ -39,6 +80,8 @@ function TeacherList() {
                 <Text style={styles.label}>Dia da semana</Text>
                 <TextInput
                   style={styles.input}
+                  value={week_day}
+                  onChangeText={text => setWeekDay(text)}
                   placeholder="Qual o dia?"
                   placeholderTextColor="#c1bccc"
                 />
@@ -48,13 +91,18 @@ function TeacherList() {
                 <Text style={styles.label}>Dia da semana</Text>
                 <TextInput
                   style={styles.input}
+                  value={time}
+                  onChangeText={text => setTime(text)}
                   placeholder="Qual horário?"
                   placeholderTextColor="#c1bccc"
                 />
               </View>
             </View>
 
-            <RectButton style={styles.submitButton}>
+            <RectButton
+              onPress={handleFiltersSubmit}
+              style={styles.submitButton}
+            >
               <Text style={styles.submitButtonText}>Filtrar</Text>
             </RectButton>
           </View>
@@ -68,10 +116,15 @@ function TeacherList() {
           paddingBottom: 16,
         }}
       >
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {teachers.map((teacher: Teacher) => {
+          return (
+            <TeacherItem
+              key={teacher.id}
+              teacher={teacher}
+              favorited={favorites.includes(teacher.id)}
+            />
+          );
+        })}
       </ScrollView>
     </View>
   );
